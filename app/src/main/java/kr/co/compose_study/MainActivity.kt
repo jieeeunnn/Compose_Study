cfe9562e6aac294395e06635ff4bd7816eb32313
Jieun <lje2000lje@naver.com>
2025-01-23 17:50:17 +0900

    design/#3: 상단바가 앱 콘텐츠 위에 겹쳐보이도록 설정

--- app/src/main/java/kr/co/compose_study/MainActivity.kt
+++ app/src/main/java/kr/co/compose_study/MainActivity.kt
@@ -8,17 +8,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import kr.co.compose_study.presentation.onboarding.OnBoardingScreen
 import kr.co.compose_study.ui.theme.Compose_StudyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
 
         setContent {
             Compose_StudyTheme {
-                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
                     OnBoardingScreen()
                 }
             }
