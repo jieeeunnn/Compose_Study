5c0905bd391ed2389e1d2cfe4d9cc1cf653309fc
Jieun <lje2000lje@naver.com>
2025-01-23 15:47:09 +0900

    chore/#1: minSdk 31 이상으로 변경 (Android 12 이상)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "kr.co.compose_study"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
