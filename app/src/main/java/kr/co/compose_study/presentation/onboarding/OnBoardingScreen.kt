45654523c6b957ea2576423aaf3a9d441dc5a901
Jieun <lje2000lje@naver.com>
2025-01-23 19:17:16 +0900

    feat/#5: onBoardingScreen 마지막 페이지에서 사용자 데이터 저장 연결

--- app/src/main/java/kr/co/compose_study/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/kr/co/compose_study/presentation/onboarding/OnBoardingScreen.kt
@@ -28,7 +28,9 @@ import kr.co.compose_study.presentation.onboarding.component.PageIndicator
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen() {
+fun OnBoardingScreen(
+    event: (OnBoardingEvent) -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -85,8 +87,8 @@ fun OnBoardingScreen() {
                     text = buttonState.value[1],
                     onClick = {
                         scope.launch {
-                            if (pagerState.currentPage == 3) {
-                                // TODO : Navigate to Home Screen
+                            if (pagerState.currentPage == 2) {
+                                event(OnBoardingEvent.SaveAppEntry)
                             } else {
                                 pagerState.animateScrollToPage(
                                     page = pagerState.currentPage + 1
