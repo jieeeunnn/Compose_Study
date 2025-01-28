ebd58c1e20db075f4ac8da22a93753aa4559d4e4
Jieun <lje2000lje@naver.com>
2025-01-28 15:24:42 +0900

    chore/#11: paging dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.gson)
+
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
 }
