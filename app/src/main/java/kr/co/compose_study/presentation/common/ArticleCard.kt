0a61268c351945672270cfd43a90761a9a725714
Jieun <lje2000lje@naver.com>
2025-01-29 21:58:57 +0900

    feat/#11: ArticleCard 컴포넌트 생성

---
+++ app/src/main/java/kr/co/compose_study/presentation/common/ArticleCard.kt
@@ -0,0 +1,122 @@
+package kr.co.compose_study.presentation.common
+
+import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import kr.co.compose_study.R
+import kr.co.compose_study.domain.model.Article
+import kr.co.compose_study.domain.model.Source
+import kr.co.compose_study.presentation.Dimens.ArticleCardSize
+import kr.co.compose_study.presentation.Dimens.ExtraSmallPadding
+import kr.co.compose_study.presentation.Dimens.ExtraSmallPadding2
+import kr.co.compose_study.presentation.Dimens.SmallIconSize
+import kr.co.compose_study.ui.theme.Compose_StudyTheme
+
+@Composable
+fun ArticleCard(
+    modifier: Modifier = Modifier,
+    article: Article,
+    onClick: () -> Unit
+) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = modifier
+            .clickable { onClick() }
+    ) {
+
+        AsyncImage(
+            modifier = Modifier
+                .size(ArticleCardSize)
+                .clip(MaterialTheme.shapes.medium),
+            model = ImageRequest.Builder(context).data(article.urlToImage).build(),
+            contentDescription = null
+        )
+
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .padding(horizontal = ExtraSmallPadding)
+                .height((ArticleCardSize))
+        ) {
+            Text(
+                text = article.title,
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(id = R.color.text_medium),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = article.source.name,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.text_medium)
+                )
+
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_time),
+                    contentDescription = stringResource(R.string.article_card_time_icon),
+                    modifier = Modifier.size(SmallIconSize),
+                    tint = Color.Black
+                )
+
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+
+                Text(
+                    text = article.publishedAt,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.text_medium)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ArticleCardPreview() {
+    Compose_StudyTheme {
+        ArticleCard(
+            article = Article(
+                author = "",
+                content = "",
+                description = "",
+                publishedAt = "2 hours",
+                source = Source(id = "", name = "BBC"),
+                title = "Her traint broke down. Her phone died. And then she met her saver i..",
+                url = "",
+                urlToImage = ""
+            )
+        ) { }
+    }
+}
