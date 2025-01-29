5e718f83479e2a66edd8c221c1c2cbd4144b4101
Jieun <lje2000lje@naver.com>
2025-01-30 01:19:19 +0900

    design/#11: HomeScreen 구현

---
+++ app/src/main/java/kr/co/compose_study/presentation/home/HomeScreen.kt
@@ -0,0 +1,97 @@
+package kr.co.compose_study.presentation.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.paging.compose.LazyPagingItems
+import kr.co.compose_study.R
+import kr.co.compose_study.domain.model.Article
+import kr.co.compose_study.presentation.Dimens.MediumPadding1
+import kr.co.compose_study.presentation.common.ArticlesList
+import kr.co.compose_study.presentation.common.SearchBar
+import kr.co.compose_study.presentation.navgraph.Route
+
+@Composable
+fun HomeScreen(
+    articles: LazyPagingItems<Article>,
+    navigate: (String) -> Unit
+) {
+    val titles by remember {
+        derivedStateOf {
+            if (articles.itemCount > 10) {
+                articles.itemSnapshotList.items
+                    .slice(IntRange(start = 0, endInclusive = 9))
+                    .joinToString(separator = " \uD83d\uDFE5") { it.title }
+            } else {
+                ""
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = MediumPadding1)
+            .statusBarsPadding()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        SearchBar(
+            modifier = Modifier.padding(horizontal = MediumPadding1),
+            text = "",
+            readOnly = true,
+            onValueChange = {},
+            onClick = {
+                navigate(Route.SearchScreen.route)
+            },
+            onSearch = {}
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        Text(
+            text = titles,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = MediumPadding1)
+                .basicMarquee(),
+            fontSize = 12.sp,
+            color = colorResource(id = R.color.text_medium)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        ArticlesList(
+            modifier = Modifier.padding(horizontal = MediumPadding1),
+            articles = articles,
+            onClick = {
+                navigate(Route.DetailsScreen.route)
+            }
+        )
+    }
+}
