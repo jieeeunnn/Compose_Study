9116cad6a0479ee1c1f3b3a42aabab200ad24d7c
Jieun <lje2000lje@naver.com>
2025-01-29 21:58:39 +0900

    feat/#11: AppModule에 api, repository, usecase 추가

--- app/src/main/java/kr/co/compose_study/di/AppModule.kt
+++ app/src/main/java/kr/co/compose_study/di/AppModule.kt
@@ -6,10 +6,18 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kr.co.compose_study.data.manager.LocalUserManagerImpl
+import kr.co.compose_study.data.remote.NewsApi
+import kr.co.compose_study.data.repository.NewsRepositoryImpl
 import kr.co.compose_study.domain.manager.LocalUserManager
+import kr.co.compose_study.domain.repository.NewsRepository
 import kr.co.compose_study.domain.usecase.app_entry.AppEntryUseCases
 import kr.co.compose_study.domain.usecase.app_entry.ReadAppEntry
 import kr.co.compose_study.domain.usecase.app_entry.SaveAppEntry
+import kr.co.compose_study.domain.usecase.news.GetNews
+import kr.co.compose_study.domain.usecase.news.NewsUseCases
+import kr.co.compose_study.util.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -30,4 +38,31 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManager),
         saveAppEntry = SaveAppEntry(localUserManager)
     )
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ) : NewsRepository = NewsRepositoryImpl(newsApi)
+
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository
+    ): NewsUseCases {
+        return NewsUseCases(
+            getNews = GetNews(newsRepository)
+        )
+    }
 }
