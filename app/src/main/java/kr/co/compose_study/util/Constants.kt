416877ff93be95ad53e68800f9fa2a2422cad855
Jieun <lje2000lje@naver.com>
2025-02-08 00:58:43 +0900

    chore/#17: 상수 추가

--- app/src/main/java/kr/co/compose_study/util/Constants.kt
+++ app/src/main/java/kr/co/compose_study/util/Constants.kt
@@ -4,4 +4,10 @@ object Constants {
     const val USER_SETTINGS = "userSettings"
 
     const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "b0752185354447e397816c287899d23e"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME = "news_db"
 }
