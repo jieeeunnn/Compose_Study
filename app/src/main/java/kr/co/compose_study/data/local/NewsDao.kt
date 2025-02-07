c598e695f1640f82dd06a68038cd3f8f141bcf24
Jieun <lje2000lje@naver.com>
2025-02-08 00:59:00 +0900

    feat/#17: NewsDao 추가

---
+++ app/src/main/java/kr/co/compose_study/data/local/NewsDao.kt
@@ -0,0 +1,23 @@
+package kr.co.compose_study.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import kr.co.compose_study.domain.model.Article
+
+@Dao
+interface NewsDao {
+
+    // 동일한 기사 삽입 시 이미 있는 기사를 업데이트
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article)
+
+    @Delete
+    suspend fun delete(article: Article)
+
+    @Query("SELECT * FROM Article")
+    fun getArticles(): Flow<List<Article>>
+}
