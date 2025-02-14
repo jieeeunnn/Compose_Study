ccdcdfcb30fe1ee1063712b82716616c6b5b883b
Jieun <lje2000lje@naver.com>
2025-02-14 15:56:52 +0900

    refactor/#21: DetailsEvent 수정 및 추가

--- app/src/main/java/kr/co/compose_study/presentation/details/DetailsEvent.kt
+++ app/src/main/java/kr/co/compose_study/presentation/details/DetailsEvent.kt
@@ -1,5 +1,8 @@
 package kr.co.compose_study.presentation.details
 
+import kr.co.compose_study.domain.model.Article
+
 sealed class DetailsEvent {
-    data object SaveArticle: DetailsEvent()
+    data class UpsertDeleteArticle(val article: Article): DetailsEvent()
+    data object RemoveSideEffect: DetailsEvent()
 }
